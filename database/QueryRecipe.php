d4fff767a885e3f6a4338961eb6648880b580b21
elizachu99 <chues@usc.edu>
2016-02-13 15:14:06 -0800

    added utterances and modify queries

--- database/QueryRecipe.php
+++ database/QueryRecipe.php
@@ -1,9 +1,4 @@
-<?php
 require 'vendor/autoload.php';
-
-date_default_timezone_set('UTC');
-
-use Aws\DynamoDb\Exception\DynamoDbException;
 
 $sdk = new Aws\Sdk([
     'region'   => 'us-east-1',
@@ -12,33 +7,16 @@ $sdk = new Aws\Sdk([
 
 $dynamodb = $sdk->createDynamoDb();
 
-$tableName = 'Recipes';
-$searchTerm = 'cheesecake';
+$response = $dynamodb->scan([
+    'TableName' => 'Recipes'
+]);
 
-# The Query API is paginated. Issue the Query request multiple times.
-do {
-    echo "Querying table $tableName for $searchTerm\n";
-
-    $request = [
-        'TableName' => $tableName,
-        'KeyConditionExpression' => 'title = :_title',
-        'ExpressionAttributeValues' =>  [
-            ':_title' => ['S' => $searchTerm]
-        ],
-    ];
-
-    $response = $dynamodb->query($request);
-
-    foreach ($response['Items'] as $key => $value) {
-        echo 'Id: ' . $value['Id']['S'] . "\n";
-        echo 'ReplyDateTime: ' . $value['ReplyDateTime']['S'] . "\n";
-        echo 'Message: ' . $value['Message']['S'] . "\n";
-        echo 'PostedBy: ' . $value['PostedBy']['S'] . "\n"; 
-        echo "\n";
-    }
-
-# If there is no LastEvaluatedKey in the response, then 
-# there are no more items matching this Query
-} while(isset($response['LastEvaluatedKey'])); 
-
-?>
+foreach ($response['Items'] as $key => $value) {
+    echo 'Title: ' . $value['title']['S'] . "\n";
+    echo 'Calories: ' . $value['calories']['N'] . "\n";
+    echo 'Servings: ' . $value['servings']['N'] . "\n";
+    echo 'Categories: ' . $value['categories']['S'] . "\n";
+    echo 'Steps: ' . $value['steps']['SS'] . "\n";
+    echo 'Ingredients: ' . $value['ingredients']['SS'] . "\n";
+    echo "\n";
+}
