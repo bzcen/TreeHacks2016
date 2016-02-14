99f4f7e82062ec23a750075dcbcf28aa8adc6ea4
Brandon Cen <bcen96@gmail.com>
2016-02-13 23:12:42 -0800

    previous step function done

--- alexa_code/src/intentHandlers.js
+++ alexa_code/src/intentHandlers.js
@@ -460,6 +460,102 @@ var registerIntentHandlers = function (intentHandlers, skillContext) {
         });
     }
 
+    intentHandlers.PreviousStepIntent = function (intent, session, response) {
+        var step = 0;
+        var id;
+        var total_steps;
+        var text;
+
+        var AWS = require("aws-sdk");
+        AWS.config.update({
+            region: "us-east-1",
+            endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+        });
+        var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+        console.log(dynamodb);
+        dynamodb.getItem({
+            TableName: 'RecipeState',
+            Key: {
+                Name: {
+                    S: "test"
+                }
+            }
+        }, function (err, data){
+            console.log(data);
+            if (err){
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                return;
+            }
+            if (data === undefined){
+                console.log("unable to find the item");
+                return;
+            } else {
+                console.log("found the item");
+
+                id = data.Item.Recipe.S;
+                step = data.Item.Step.N;
+                console.log(id);
+                dynamodb.getItem({
+                    TableName: 'Recipes',
+                    Key: {
+                        title: {
+                            S: id
+                        }
+                    }
+                }, function (err, data){
+                    if (err){
+                        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                    }
+                    if (data === undefined){
+                        console.log("unable to find the item");
+
+                    }else {
+                        total_steps = data.Item.steps.L.length;
+
+                        step = step - 2;
+
+                        if (step <= -2){
+                            response.ask("It looks like you haven't started cooking yet", "Would you like to cook?");
+                            return;
+                        } if (step === -1){
+                            step = 0;
+                        } 
+                            var list = data.Item.steps.L;
+                            text = list[step].S;
+
+                            step++;
+                            dynamodb.putItem({
+                            TableName: 'RecipeState',
+                            Item: {
+                            Name: {
+                                S: 'test'
+                            }   ,
+                            Recipe: {
+                                S: id
+                            },
+                                Step: {
+                                    N: step.toString()
+                                }
+
+                                }
+                            }, function (err, data) {
+                                if (err){
+                                console.log(err);
+                                }
+                                else {
+                                    
+                                    response.ask(text, "Do you want to continue?");
+                                }
+                            
+                            });
+                        }
+                });
+
+            }
+        });
+    }
+
     intentHandlers.RepeatStepIntent = function (intent, session, response) {
 
         var step = 0;
@@ -537,6 +633,7 @@ var registerIntentHandlers = function (intentHandlers, skillContext) {
         });
 
     }
+
 
 
 
